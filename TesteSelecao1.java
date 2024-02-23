804d8341d1cc833546f0e844309a77581b28c981
Leticia Florêncio <70420362+Leeflorencio@users.noreply.github.com>
2024-02-23 00:14:43 -0300

    Update TesteSelecao1.java

--- TesteSelecao1.java
+++ TesteSelecao1.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 public class TesteSelecao1 {
 
     public static void main(String[] args) {
-        //Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A,
-        // e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a
-        // variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
-
 
         Scanner sc = new Scanner(System.in);
 
